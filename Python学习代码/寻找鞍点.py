e38e7a02d1e14439a7ee41ca3960a762fa5334c8
TYHTTT3850 <118507779+TYHTTT3850@users.noreply.github.com>
2024-08-22 22:09:04 +0800

    Add files via upload

---
+++ Python学习代码/寻找鞍点.py
@@ -0,0 +1,21 @@
+import numpy as np
+B = []
+A = np.array([[9,80,205,40],[90,-60,96,1],[210,-3,101,89]])
+print(A)
+rows,columns = A.shape
+print(f"行：{rows},列：{columns}")
+for i in range(rows):
+    max = A[i][0]
+    for j in range(0,columns):
+        if A[i][j] > max:
+            max = A[i][j]
+    B.append(max)
+print("每行的最大值为：\n",B)
+for i in range(rows):
+    x,y = np.where(A == B[i])
+    min = A[0,y[0]]
+    for j in range(0,rows):
+        if A[j,y[0]] <= min:
+            min = A[j,y[0]]
+    if min == B[i]:
+        print(f"{B[i]}为鞍点")
