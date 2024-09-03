c25001890c2c2b3b7e8d2c986e350db32c85da52
TYHTTT3850 <118507779+TYHTTT3850@users.noreply.github.com>
2024-09-03 20:16:56 +0800

    Add files via upload

---
+++ Python学习代码/创建和导入自定义模块/创建和导入自定义模块.py
@@ -0,0 +1,11 @@
+"""创建自定义模块：即创建python代码文件，在文件中写好各种功能。"""
+
+# import MyModule1
+# MyModule1.test(5,1)
+
+# import MyModule2
+# MyModule2.test(5,2)
+
+# from MyModule1 import test
+# from MyModule2 import test
+# test(5,2) # 从不同模块导入同名功能，执行方法以最后导入的为准。后导入的覆盖先导入的
