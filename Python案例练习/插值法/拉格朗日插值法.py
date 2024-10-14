a67a268064a3c2ff68a9c8916c80de1d62c8c4b9
TYHTTT3850 <118507779+TYHTTT3850@users.noreply.github.com>
2024-10-14 19:13:43 +0800

    Add files via upload

---
+++ Python案例练习/插值法/拉格朗日插值法.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def lagrange(u,X,Y): #拉格朗日插值法，u:插值点，X:插值区间，Y:插值区间函数值
+    L = np.ones(len(X))
+    for j in range(len(X)):#得到插值基函数
+        for i in range(len(X)):
+            if i == j:
+                continue
+            else:
+                L[j] *= (u-X[i])/(X[j]-X[i])
+    f = np.sum(Y*L)
+    return f
+
+x = np.array([144,169,225])
+y = np.array([12,13,15])
+print(lagrange(175,x,y))
