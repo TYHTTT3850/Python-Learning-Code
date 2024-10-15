7b60c99ab94c7ff45153805581d17220c41dc955
TYHTTT3850 <118507779+TYHTTT3850@users.noreply.github.com>
2024-10-15 23:00:26 +0800

    Update Newton插值法.py

--- Python案例练习/插值法/Newton插值.py
+++ Python案例练习/插值法/Newton插值.py
@@ -1,23 +1,22 @@
-"""
-已知 ln(x) 在 0.4 0.5 0.6 0.7 0.8处的值，求在 0.54 处的近似值
-"""
-
 import numpy as np
+def newton(u,X,Y,order): #牛顿插值法，u:插值点，X:已知点，Y:已知点函数值，order:插值阶数
+    n = len(X)
+    if order >= n:
+        print("阶数错误")
+    else:
+        table = np.zeros([n,n])
+        table[:,0] = Y
+        for k in range(1,n):
+            for i in range(k,n):
+                table[i,k] = (table[i,k-1]-table[i-1,k-1])/(X[i] - X[i-k])
+        N = table[0,0] #初始化为常数项
+        x_term = 1 #初始化含x的项为零次幂
+        for power in range(1,order+1): #power表示现在插值到x的第几幂次
+            x_term *= u-X[power-1]
+            N += table[power,power]*x_term
+        print(table)
+        print(N)
 
-# 取0.5，0.6，0.4三个点
-diff_x = np.array([[0.5],[0.6],[0.4]])
-diff_table = np.array([[-0.6931],[-0.5108],[-0.9163]])
-
-# 下三角系数阵
-A = np.tril(np.ones([len(diff_x),len(diff_x)]))
-for i in range(1,len(diff_x)):
-    for j in range(i,len(diff_x)):
-        A[j,i] = (diff_x[j,0] - diff_x[i-1,0])*(A[j,i-1])
-
-# 解出系数
-a = np.linalg.solve(A,diff_table)
-
-# 求近似值
-x = 0.54
-print(a[0]+a[1]*(x-diff_x[0])) # 线性插值
-print(a[0]+a[1]*(x - diff_x[0])+a[2]*(x - diff_x[0])*(x - diff_x[1])) # 抛物线插值
+x = np.array([0.5,0.6,0.4])
+y = np.array([-0.6931,-0.5108,-0.9163,])
+newton(0.54,x,y,2)
