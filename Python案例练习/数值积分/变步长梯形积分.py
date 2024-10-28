04300966ae895e1b435c101e59faab2e5d120915
TYHTTT3850 <118507779+TYHTTT3850@users.noreply.github.com>
2024-10-28 19:14:01 +0800

    Update 变步长梯形积分.py

--- Python案例练习/数值积分/变步长梯形积分.py
+++ Python案例练习/数值积分/变步长梯形积分.py
@@ -1,18 +1,29 @@
 import numpy as np
 
 def Trapezoidal_integral(f,a,b,eps):#f:积分函数，a:积分下限，b:积分上限，eps:精度控制
-    T = np.zeros(int(1/eps))
-    T[1] = (b-a)*(f(a)+f(b))/2 #分段区间为1时积分近似值
-    for i in range(1,int(1/eps)): #从分为2段区间开始
-        n = 2**i #表示分段区间数
-        h = (b-a)/n #分段区间间隔
-        for t2 in range(0,n):
-            T[i] += h*(f(a+t2*h)+f(a+(t2+1)*h))/2 #分段梯形求积
-        if np.abs(T[i]-T[i-1]) < eps:
-            return n, T[i]
+    #初始化T1和T2
+    T1 = (b-a)*(f(a)+f(b))/2
+    T2 = 0
+
+    #从分为2段区间的情况开始
+    for i in range(1, int(1 / eps)):
+        n = 2 ** i  #表示分段区间数
+        h = (b - a) / n  #表示分段区间间隔
+
+        # 分段梯形求积
+        for t2 in range(0, n): #遍历所有分段区间
+            T2 += h * (f(a + t2 * h) + f(a + (t2 + 1) * h)) / 2
+
+        # 判断结果是否符合要求
+        if np.abs(T2 - T1) < eps:
+            return n, T2
+            break #返回后退出循环
         else:
+            T1 = T2 #T2的值保存至T1
+            T2 = 0 #重置T2的值
             continue
 
+# 定义求积函数
 def f(x):
     if x == 0:
         return 1
